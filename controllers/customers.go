9123fdd6f1dbcd7f2c1091574b698d092524a3c5
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-12-13 21:45:11 +0100

    add GetCustomerByID

--- controllers/customers.go
+++ controllers/customers.go
@@ -2,9 +2,14 @@ package controllers
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 
+	"github.com/ctrlmaniac/gocrm/models"
 	"github.com/ctrlmaniac/gocrm/persistance"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 )
 
 func GetCustomers(w http.ResponseWriter, r *http.Request) {
@@ -12,4 +17,47 @@ func GetCustomers(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	json.NewEncoder(w).Encode(persistance.Data)
+}
+
+func CreateCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var newEntry map[string]string
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	json.Unmarshal(reqBody, &newEntry)
+
+	if _, hasID := newEntry["ID"]; hasID {
+		// if request has ID it will return an error
+		w.WriteHeader(http.StatusConflict)
+	} else {
+		contacted, _ := strconv.ParseBool(newEntry["Contacted"])
+
+		persistance.Data = append(persistance.Data, models.Customer{
+			ID:        uuid.New().String(),
+			Name:      newEntry["Name"],
+			Role:      newEntry["Role"],
+			Email:     newEntry["Email"],
+			Phone:     newEntry["Phone"],
+			Contacted: contacted,
+		})
+
+		json.NewEncoder(w).Encode(persistance.Data)
+	}
+}
+
+func GetCustomerByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id := mux.Vars(r)["id"]
+
+	for _, c := range persistance.Data {
+		if c.ID == id {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(c)
+			break
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
 }
--- main/main.go
+++ main/main.go
@@ -12,6 +12,8 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/customers", controllers.GetCustomers).Methods("GET")
+	router.HandleFunc("/customers", controllers.CreateCustomer).Methods("POST")
+	router.HandleFunc("/customers/{id}", controllers.GetCustomerByID).Methods("GET")
 
 	fmt.Println("Server is running at http://localhost:3000/")
 	http.ListenAndServe(":3000", router)
